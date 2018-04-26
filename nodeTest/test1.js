bd07688437528f4a79b864a4b0ccb5f7ecd9ae8e
范翔 <fanxiang@beeplay123.com>
2018-04-26 14:37:03 +0800

    2

--- nodeTest/test1.js
+++ nodeTest/test1.js
@@ -9,7 +9,10 @@ test();
 var a = require('./moduleT');//a == 对象{a:mo}  或者理解为： module.exports == a
 var b = require('./exportsT');// b == exports ,但是exports必须挂载一个属性才可以抛出去
 var c = require('./exportsTT');//c == exports
-var e = require('httpGet');
+
+if(process.env.doWhat == 'http'){
+    var e = require('httpGet');
+}
 // a.mo()
 // b.ex.ex();
 // c.hello();
