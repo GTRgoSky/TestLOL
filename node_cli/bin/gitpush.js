f261a7d6a456e138d306b4cb0539496e60d20e40
Wings <fanxiang@beeplay123.com>
2018-09-14 13:48:08 +0800

    '测试用promise处理脚本3'

--- node_cli/bin/gitpush.js
+++ node_cli/bin/gitpush.js
@@ -4,35 +4,37 @@
 
 const util = require('util');
 const exec = util.promisify(require('child_process').exec);
-const execSync = require('child_process').execSync;
+// const execSync = require('child_process').execSync;
 
-try {
-    const str1 = execSync('git status', {encoding: 'utf8'});
-    const re = str1.match(/^On branch (.+)\s/);
-    console.log('你现在所在分支：' + re[1] + '\n');
-    execSync('git add *', {encoding: 'utf8'});
-    const str2 = execSync('git commit ' + '-m '+ process.argv[2], {encoding: 'utf8'});
-    console.log(str2);
-    const str3 = execSync('git push', {encoding: 'utf8'});
-    // const str3 = execSync('git pull origin ' + re[1], {encoding: 'utf8'});
-} catch (err) {
-    console.log('execute error：');
-    console.log(err.toString());
-}
-
-// async function pushGit() {
-//     console.log('start-inner')
-//     const { stdout:str1, stderr:stderr1 } = await exec('git status', {encoding: 'utf8'});
-//     console.log(str1)
-//     if(stderr1) console.log("stderr1:",stderr1);
+// try {
+//     const str1 = execSync('git status', {encoding: 'utf8'});
 //     const re = str1.match(/^On branch (.+)\s/);
 //     console.log('你现在所在分支：' + re[1] + '\n');
-//     exec('git add *', {encoding: 'utf8'});
-//     execSync('git commit ' + '-m '+ process.argv[2], {encoding: 'utf8'});
-//     await exec('git push', {encoding: 'utf8'});
-//     console.log('end-inner')
+//     execSync('git add *', {encoding: 'utf8'});
+//     const str2 = execSync('git commit ' + '-m '+ process.argv[2], {encoding: 'utf8'});
+//     console.log(str2);
+//     const str3 = execSync('git push', {encoding: 'utf8'});
+//     // const str3 = execSync('git pull origin ' + re[1], {encoding: 'utf8'});
+// } catch (err) {
+//     console.log('execute error：');
+//     console.log(err.toString());
 // }
 
-// console.log('start fun')
-// pushGit();
-// console.log('end fun')
+async function pushGit() {
+    console.log('start-inner')
+    const { stdout:str1, stderr:stderr1 } = await exec('git status', {encoding: 'utf8'});
+    console.log(str1)
+    if(stderr1) console.log("stderr1:",stderr1);
+    const re = str1.match(/^On branch (.+)\s/);
+    console.log('你现在所在分支：' + re[1] + '\n');
+    await exec('git add *', {encoding: 'utf8'});
+    const { stdout:str2, stderr:stderr2 } = await exec('git commit ' + '-m '+ process.argv[2], {encoding: 'utf8'});
+    console.log(str2);
+    if(stderr2) console.log("stderr2:",stderr1);
+    await exec('git push', {encoding: 'utf8'});
+    console.log('end-inner')
+}
+
+console.log('start fun')
+pushGit();
+console.log('end fun')
