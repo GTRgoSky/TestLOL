5538b420e447d4ed78a8e2527b0c7db3f158ec6b
WIngs_kd <fanxiang@kedacom.com>
2019-01-22 10:49:15 +0800

    1

--- npm-release/readme.md
+++ npm-release/readme.md
@@ -2,3 +2,16 @@
 https://segmentfault.com/a/1190000007665813
 https://juejin.im/post/5bdfa46951882516c94e5b26
 npm 
+
+timeFormat (str)
+传入YYYY-MM-DD 或者 YYYY-MM-DD hh:mm:ss 获取当前时间
+
+timeSetFormat (date, str)
+格式化导入时间。第一参数传入new Date()可以解析的时间 第二参数传入YYYY-MM-DD 或者 YYYY-MM-DD hh:mm:ss
+
+whichType(v)
+判断对象类型
+
+timeCompare
+比较2个时间大小(v1 > v2 返回 1 ； v1 == v2 返回 0 ； v1 < v2 返回 -1)
+参数格式最好为入YYYY-MM-DD 或者 YYYY-MM-DD hh:mm:ss 否则可能引起未知错误
--- tSProject/hello-ts-vue/src/views/About.vue
+++ tSProject/hello-ts-vue/src/views/About.vue
@@ -1,5 +1,30 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <mu-container>
+      <mu-flex justify-content="between" align-items="end" wrap="wrap">
+        <mu-paper :z-depth="1" class="demo-date-picker">
+          <mu-date-picker :date.sync="date"></mu-date-picker>
+        </mu-paper>
+        <mu-paper :z-depth="1" class="demo-date-picker">
+          <mu-date-picker landscape :date.sync="date"></mu-date-picker>
+        </mu-paper>
+      </mu-flex>
+    </mu-container>
   </div>
 </template>
+
+<script>
+export default {
+  data () {
+    return {
+      date: undefined
+    };
+  }
+}
+</script>
+<style>
+.demo-date-picker {
+  margin: 8px;
+}
+</style>
